f495474a1f7a9a4bf72c540c7f73ae689ec49217
Florian Reiss <85802@supinfo.com>
2013-07-12 17:46:24 -0700

    Adding tableView
    Adding a tableView to countain information.

--- VideoFix/CenterViewController.h
+++ VideoFix/CenterViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CenterViewController : UIViewController
+@interface CenterViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- VideoFix/CenterViewController.m
+++ VideoFix/CenterViewController.m
@@ -15,60 +15,104 @@
 
 @implementation CenterViewController
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+{
     return YES;
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     if(!self.title) self.title = @"Demo!";
     
     [self setupMenuBarButtonItems];
 }
 
-- (MFSideMenuContainerViewController *)menuContainerViewController {
+- (MFSideMenuContainerViewController *)menuContainerViewController
+{
     return (MFSideMenuContainerViewController *)self.navigationController.parentViewController;
 }
 
-
-#pragma mark -
 #pragma mark - UIBarButtonItems
 
-- (void)setupMenuBarButtonItems {
-    if(self.menuContainerViewController.menuState == MFSideMenuStateClosed &&
-       ![[self.navigationController.viewControllers objectAtIndex:0] isEqual:self]) {
+- (void)setupMenuBarButtonItems
+{
+    if(self.menuContainerViewController.menuState == MFSideMenuStateClosed && ![[self.navigationController.viewControllers objectAtIndex:0] isEqual:self]) {
         self.navigationItem.leftBarButtonItem = [self backBarButtonItem];
-    } else {
+    }
+    else {
         self.navigationItem.leftBarButtonItem = [self leftMenuBarButtonItem];
     }
 }
 
-- (UIBarButtonItem *)leftMenuBarButtonItem {
-    return [[UIBarButtonItem alloc]
-            initWithImage:[UIImage imageNamed:@"menu-icon.png"] style:UIBarButtonItemStyleBordered
-            target:self
-            action:@selector(leftSideMenuButtonPressed:)];
+- (UIBarButtonItem *)leftMenuBarButtonItem
+{
+    return [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menu-icon.png"] style:UIBarButtonItemStyleBordered target:self action:@selector(leftSideMenuButtonPressed:)];
 }
 
-- (UIBarButtonItem *)backBarButtonItem {
-    return [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back-arrow"]
-                                            style:UIBarButtonItemStyleBordered
-                                           target:self
-                                           action:@selector(backButtonPressed:)];
+- (UIBarButtonItem *)backBarButtonItem
+{
+    return [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back-arrow"] style:UIBarButtonItemStyleBordered target:self action:@selector(backButtonPressed:)];
 }
 
-
-#pragma mark -
 #pragma mark - UIBarButtonItem Callbacks
 
-- (void)backButtonPressed:(id)sender {
+- (void)backButtonPressed:(id)sender
+{
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-- (void)leftSideMenuButtonPressed:(id)sender {
+- (void)leftSideMenuButtonPressed:(id)sender
+{
     [self.menuContainerViewController toggleLeftSideMenuCompletion:^{
         [self setupMenuBarButtonItems];
     }];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [NSString stringWithFormat:@"Section %d", section];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Item %d", indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    /*
+    CenterViewController *demoController = [[CenterViewController alloc] initWithNibName:@"CenterViewController" bundle:nil];
+    demoController.title = [NSString stringWithFormat:@"Demo #%d-%d", indexPath.section, indexPath.row];
+    
+    UINavigationController *navigationController = self.menuContainerViewController.centerViewController;
+    NSArray *controllers = [NSArray arrayWithObject:demoController];
+    navigationController.viewControllers = controllers;
+    [self.menuContainerViewController setMenuState:MFSideMenuStateClosed];
+     */
 }
 
 @end
