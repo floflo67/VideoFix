b032d8141b4dc7e2e4e2ae298568effb61616c0c
Florian Reiss <85802@supinfo.com>
2013-07-19 22:59:59 -0700

    Adding TopicsViewController
    Adding TopicsViewController. Adding a tableview inside.

---
+++ VideoFix/TopicsViewController.h
@@ -0,0 +1,14 @@
+//
+//  TopicsViewController.h
+//  VideoFix
+//
+//  Created by Florian Reiss on 19/07/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ VideoFix/TopicsViewController.m
@@ -0,0 +1,27 @@
+//
+//  TopicsViewController.m
+//  VideoFix
+//
+//  Created by Florian Reiss on 19/07/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "TopicsViewController.h"
+
+@interface TopicsViewController ()
+
+@end
+
+@implementation TopicsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
