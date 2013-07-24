3ec5840d8f7e58668ca4701b773d4cf333ccf017
Florian Reiss <85802@supinfo.com>
2013-07-24 09:54:32 -0700

    New object : question

---
+++ VideoFix/Objects/Question.h
@@ -0,0 +1,13 @@
+//
+//  Question.h
+//  VideoFix
+//
+//  Created by Florian Reiss on 24/07/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Question : NSObject
+
+@end
