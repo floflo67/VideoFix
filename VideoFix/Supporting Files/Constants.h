29b34eda465bacf6fb6c041729e9403ceb815a5d
Florian Reiss <85802@supinfo.com>
2013-07-24 18:22:23 -0700

    Adding constants file

---
+++ VideoFix/Supporting Files/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  VideoFix
+//
+//  Created by Florian Reiss on 24/07/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#ifndef VideoFix_Constants_h
+#define VideoFix_Constants_h
+
+
+
+#endif
