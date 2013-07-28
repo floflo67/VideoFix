c7f64e2bbf09541428dc7a5e65d482d960501222
seb <seb.in.san.jose@gmail.com>
2013-07-28 15:04:21 -0700

    Adding framework + constants

---
+++ VideoFix/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  VideoFix
+//
+//  Created by Sebastien WICKERT on 7/28/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#ifndef VideoFix_Constants_h
+#define VideoFix_Constants_h
+
+#define VF_API_URL @("http://sebastien-wickert.com/VF/getMostRecentQuestions.php?token=f9fde950aafbb8a6b32a6a0c33029870")
+
+
+#endif
