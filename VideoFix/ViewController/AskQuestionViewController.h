0d0ecf9aec63658b97ca512e5784183d93560d65
Florian Reiss <85802@supinfo.com>
2013-08-07 18:28:14 -0700

    Adding AskQuestionViewController
    Adding AskQuestionViewController as a imagePickerController.

---
+++ VideoFix/ViewController/AskQuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  AskQuestionViewController.h
+//  VideoFix
+//
+//  Created by Florian Reiss on 07/08/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AskQuestionViewController : UIImagePickerController
+
+@end
